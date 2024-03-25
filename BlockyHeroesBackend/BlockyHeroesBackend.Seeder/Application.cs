fdff68dcdd0ed2749d0cae141637a30056ac45a3
jrg96 <gomezlopez.jorge96@gmail.com>
2024-03-24 20:40:03 -0700

    Created Seeder to add sample data for BlockyHeroesDb
    -> Created basic seeder logic to add Equips to database
    -> Created equipment.json file to add new sample equipment

---
+++ BlockyHeroesBackend/BlockyHeroesBackend.Seeder/Application.cs
@@ -0,0 +1,45 @@
+﻿using BlockyHeroesBackend.Domain.Common.ValueObjects.Equip;
+using BlockyHeroesBackend.Domain.Entities.Equip;
+using BlockyHeroesBackend.Domain.Repositories;
+using BlockyHeroesBackend.Domain.Repositories.Command;
+using System.Text.Json;
+
+namespace BlockyHeroesBackend.Seeder;
+
+public class Application
+{
+    private readonly IEquipCommandRepository _equipCommandRepository;
+    private readonly IUnitOfWork _unitOfWork;
+
+    public Application(
+        IEquipCommandRepository equipCommandRepository,
+        IUnitOfWork unitOfWork)
+    {
+        _equipCommandRepository = equipCommandRepository;
+        _unitOfWork = unitOfWork;
+    }
+
+    public async Task RunSeeder()
+    {
+        await LoadEquips();
+    }
+
+    private async Task LoadEquips()
+    {
+        string data = File.ReadAllText("Samples/equipment.json");
+        var equips = JsonSerializer.Deserialize<List<Equip>>(data);
+        
+        foreach(var eq in equips)
+        {
+            eq.Id = EquipId.CreateEquipId();
+            foreach (var equipLevel in eq.EquipmentEvolutions)
+            {
+                equipLevel.Id = EquipLevelId.CreateEquipId();
+            }
+
+            await _equipCommandRepository.InsertAsync(eq);
+        }
+
+        await _unitOfWork.SaveChangesAsync();
+    }
+}
---
+++ BlockyHeroesBackend/BlockyHeroesBackend.Seeder/Program.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using BlockyHeroesBackend.Infrastructure.Extensions;
+using BlockyHeroesBackend.Infrastructure.Context;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using BlockyHeroesBackend.Seeder;
+
+
+/*
+ * DI Container initialization
+ */
+var builder = new ConfigurationBuilder();
+var service = new ServiceCollection();
+builder.SetBasePath(Directory.GetCurrentDirectory())
+       .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+IConfiguration config = builder.Build();
+
+service.AddDbContext<BlockyHeroesDbContext>(options =>
+    options.UseSqlServer(config.GetConnectionString("BlockyHeroesDb")));
+service.AddInfrastructure();
+service.AddSingleton<Application>();
+
+var serviceProvider = service.BuildServiceProvider();
+
+Application app = serviceProvider.GetRequiredService<Application>();
+await app.RunSeeder();
