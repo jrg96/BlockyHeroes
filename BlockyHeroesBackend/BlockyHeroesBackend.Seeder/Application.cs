63c5fc77ce7262db20d62a2f8f291f63382c05a0
jrg96 <gomezlopez.jorge96@gmail.com>
2024-03-24 21:08:50 -0700

    Database Seeder: Create fake user accounts
    -> Changed BlockyHeroesBackend.Seeder to create fake user accounts
    -> Added new equipments in equipment.json file

--- BlockyHeroesBackend/BlockyHeroesBackend.Seeder/Application.cs
+++ BlockyHeroesBackend/BlockyHeroesBackend.Seeder/Application.cs
@@ -1,5 +1,8 @@
-﻿using BlockyHeroesBackend.Domain.Common.ValueObjects.Equip;
+﻿using BlockyHeroesBackend.Application.Services;
+using BlockyHeroesBackend.Domain.Common.ValueObjects.Equip;
+using BlockyHeroesBackend.Domain.Common.ValueObjects.User;
 using BlockyHeroesBackend.Domain.Entities.Equip;
+using BlockyHeroesBackend.Domain.Entities.User;
 using BlockyHeroesBackend.Domain.Repositories;
 using BlockyHeroesBackend.Domain.Repositories.Command;
 using System.Text.Json;
@@ -9,19 +12,63 @@ namespace BlockyHeroesBackend.Seeder;
 public class Application
 {
     private readonly IEquipCommandRepository _equipCommandRepository;
+    private readonly IUserCommandRepository _userCommandRepository;
+    
     private readonly IUnitOfWork _unitOfWork;
+    private readonly IUserSecurityService _userSecurityService;
+
+    private List<User> _users = new List<User>();
 
     public Application(
         IEquipCommandRepository equipCommandRepository,
-        IUnitOfWork unitOfWork)
+        IUserCommandRepository userCommandRepository,
+        IUnitOfWork unitOfWork, 
+        IUserSecurityService userSecurityService)
     {
         _equipCommandRepository = equipCommandRepository;
+        _userCommandRepository = userCommandRepository;
         _unitOfWork = unitOfWork;
+        _userSecurityService = userSecurityService;
     }
 
     public async Task RunSeeder()
     {
+        Console.WriteLine("Building user list");
+        await LoadUsers();
+
+        Console.WriteLine("Building Equipment List");
         await LoadEquips();
+
+        Console.WriteLine("Sample data loaded successfully!");
+    }
+
+    private async Task LoadUsers()
+    {
+        // For this seeder default password will be ABBBCCDDSSSD
+        string defaultPassword = "ABBBCCDDSSSD";
+        (byte[] salt, string hash) = _userSecurityService.HashPassword(defaultPassword);
+
+        for (int i=0; i < 5; i++)
+        {
+            UserId id = UserId.CreateUserId();
+            var user = new User()
+            {
+                Id = id,
+                Name = $"Player{i.ToString()}",
+                Email = $"{id.ToString()}@blockyheroes.com",
+                Coins = 100000,
+                Password = hash,
+                Salt = salt,
+                Role = Roles.User,
+                Stamina = 80,
+                MaxStamina = 80
+            };
+
+            await _userCommandRepository.InsertAsync(user);
+            _users.Add(user);
+        }
+
+        await _unitOfWork.SaveChangesAsync();
     }
 
     private async Task LoadEquips()
