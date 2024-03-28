b426f160bfb23adf9b7fff390c2e909f3ba278e1
jrg96 <gomezlopez.jorge96@gmail.com>
2024-03-27 18:11:33 -0700

    Domain: Added record type to represent different rarities in game

---
+++ BlockyHeroesBackend/BlockyHeroesBackend.Domain.Common/ValueObjects/Common/ItemRarity.cs
@@ -0,0 +1,29 @@
+﻿namespace BlockyHeroesBackend.Domain.Common.ValueObjects.Common;
+
+public record ItemRarity(int Id, string Name)
+{
+    public static ItemRarity SuperSuperRare { get; } = new ItemRarity(1, "Super Super rare");
+    public static ItemRarity SuperRare { get; } = new ItemRarity(2, "Super Rare");
+    public static ItemRarity Rare { get; } = new ItemRarity(3, "Rare");
+    public static ItemRarity Common { get; } = new ItemRarity(4, "Common");
+
+    public static ItemRarity Get(int id)
+    {
+        if (SuperSuperRare.Id == id)
+        {
+            return SuperSuperRare;
+        }
+        else if(SuperRare.Id == id)
+        {
+            return SuperRare;
+        }
+        else if(Rare.Id == id)
+        {
+            return Rare;
+        }
+        else
+        {
+            return Common;
+        }
+    }
+}
